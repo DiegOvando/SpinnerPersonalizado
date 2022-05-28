bba6a94c8fdace5eeb0aab92ff658f9339f9352e
Diego Ovando <c9sneaky222@hotmail.com>
2022-05-28 01:28:14 -0600

    Button añadido
    Se agregó el botón de "Cerrar".

--- SpinnerPersonalizado/app/src/main/java/com/example/spinnerpersonalizado/MainActivity.java
+++ SpinnerPersonalizado/app/src/main/java/com/example/spinnerpersonalizado/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.spinnerpersonalizado;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.Toast;
 import java.util.ArrayList;
@@ -10,6 +14,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
     Spinner sp;
+    private Button btnCerrar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,6 +39,28 @@ public class MainActivity extends AppCompatActivity {
             }
             @Override
             public void onNothingSelected(AdapterView<?> adapterView) {
+            }
+        });
+
+        btnCerrar = (Button)findViewById(R.id.btnCerrar);
+        btnCerrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder confirmar = new AlertDialog.Builder(MainActivity.this);
+                confirmar.setTitle("¿Cerrar App?");
+                confirmar.setMessage("Se descartara toda la informacion. ");
+                confirmar.setPositiveButton("Confirmar", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                    }
+                });
+                confirmar.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //Nada
+                    }
+                }); confirmar.show();
             }
         });
     }
